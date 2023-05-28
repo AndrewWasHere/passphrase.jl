d9563819a9920dd9bcc7e3382af1da7f8e8ff211
Andrew Lin <andrew.lin@ieee.org>
2023-05-28 11:35:23 -0700

    Added help, multiple file support.

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Random words in a range of lengths. Written in Julia.
 
 Use:
 ```shell
-julia passphrase.jl 
+julia passphrase.jl
     [ -n <Number of words> ]  # default is 5
     [ --min <minimum number of characters in word> ]  # default is 2
     [ --max <maximum number of characters in word> ]  # default is 6
-    [ file to read words from ]  # default is /usr/share/dict/words
+    [ words file [words file] ... ]  # default is /usr/share/dict/words
 ```
-Word file is assumed to have one word per line.
+Words files are assumed to have one word per line.
 
 ## Dependencies
 
--- passphrase.jl
+++ passphrase.jl
@@ -1,31 +1,47 @@
 using Random
 
-mutable struct Args
+struct Args
     n::Int 
     length_min::Int 
     length_max::Int 
-    filepath::AbstractString
+    filepaths::Vector{<:AbstractString}
 end
 
 
+function show_help()
+    println("Usage: julia passpharse.jl [-n N] [--min N] [--max N] [-h] [[file] [file] ...]")
+    println("Where:")
+    println("  -n N        Set number of words to show to N (default is 5)")
+    println("  --min N     Set minimum number of characters in a word to N (default is 2)")
+    println("  --max N     Set maximum number of characters in a word to N (default is 6)")
+    println("  -h, --help  Show this help and exit")
+    println("  file        Path to file containing words list (default is /usr/share/dict/words)")
+end
+
 function parse_args(args::Vector{<:AbstractString})
     # Default settings.
-    settings = Args(5, 2, 6, "/usr/share/dict/words")
+    n = 5
+    length_min = 2
+    length_max = 6
+    filepaths = ["/usr/share/dict/words"]
 
     function set_n(value::AbstractString)
-        settings.n = parse(Int, value)
+        n = parse(Int, value)
+        f = set_file
     end
 
     function set_min(value::AbstractString)
-        settings.length_min = parse(Int, value)
+        length_min = parse(Int, value)
+        f = set_file
     end
 
     function set_max(value::AbstractString)
-        settings.length_max = parse(Int, value)
+        length_max = parse(Int, value)
+        f = set_file
     end
 
     function set_file(value::AbstractString)
-        settings.filepath = value
+        push!(filepaths, value)
     end
 
     f = set_file
@@ -36,14 +52,18 @@ function parse_args(args::Vector{<:AbstractString})
             f = set_min
         elseif a == "--max"
             f = set_max
+        elseif a in ("-h", "--help")
+            show_help()
+            exit(0)
         else
             f(a)
         end
     end
-    return settings
+
+    return Args(n, length_min, length_max, filepaths)
 end
 
-function load_words(path::AbstractString, min::Int, max::Int)
+function load_words(paths::Vector{<:AbstractString}, min::Int, max::Int)
     function acceptable(w::AbstractString)
         acceptable_length = min <= length(w) <= max
         acceptable_chars = ! occursin("'", w)
@@ -51,15 +71,20 @@ function load_words(path::AbstractString, min::Int, max::Int)
         return acceptable_length && acceptable_chars
     end
 
-    [word for word in eachline(path) if acceptable(word)]
+    words = Vector{String}()
+    for p in paths
+        append!(words, [word for word in eachline(p) if acceptable(word)])
+    end
+
+    return words
 end
 
 function main()
     args = parse_args(ARGS)
-    words = load_words(args.filepath, args.length_min, args.length_max)
+    words = load_words(args.filepaths, args.length_min, args.length_max)
     shuffle!(words)
     
-    println(join(words[begin:args.n], " "))
+    println(join(words[begin:min(args.n, length(words))], " "))
 end
 
 main()
